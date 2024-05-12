863ef569ba21513e99666566617ecca4a118e618
Ibrahim <ibz_374@hotmail.com>
2024-05-12 01:29:25 +0100

    Working on skills page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import image from '../assets/personal-img.jpg'
 import '../styles/Home.css'
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 const Home = () => {
     const personalInfo = {
@@ -20,7 +22,10 @@ const Home = () => {
       <div className='txt-container'>
       <h2>Hi there, my name is <span className='span1'>{personalInfo.firstName + ' ' + personalInfo.lastName}</span></h2>
       <h3 className='span2'>I am a <span>{titles[selectedTitle]}</span></h3>
-      <button>Contact me <i class="bi bi-envelope"></i></button>
+      </div>
+      <div className='btn-links'>
+      <a href='https://github.com/Mullah98'><button className='github'><GitHubIcon /></button></a>
+      <a href='https://www.linkedin.com/in/ibrahim-ullah-380972284'><button className='linkdin'><LinkedInIcon /></button></a>
       </div>
     </div>
   )
--- src/pages/TechStack.jsx
+++ src/pages/TechStack.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import '../styles/TechStack.css'
 
 const TechStack = () => {
+  const frontend = [
+    {name: 'HTML', icon: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'},
+    {name: 'CSS', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png'},
+    {name: 'React', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png'},
+    {name: 'React Native', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTw3HApB4bsvabXW3L14cV-LhFo0L71QmEESJN3vW9Ow&s'},
+    {name: 'Sass', icon: 'https://sass-lang.com/assets/img/styleguide/seal-color.png'},
+  ]
+
+  const backend = [
+    {name: 'SQL', icon: 'https://logowik.com/content/uploads/images/azure-sql-database6354.jpg'},
+    {name: 'PostgreSQL', icon: 'https://download.logo.wine/logo/PostgreSQL/PostgreSQL-Logo.wine.png'},
+    {name: 'Node.js', icon: 'https://www.svgrepo.com/download/303360/nodejs-logo.svg'},
+    {name: 'Express', icon: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png'},
+    {name: 'REST API', icon: 'https://www.opc-router.de/wp-content/uploads/2020/05/REST_socialmedia.jpg'},
+  ]
+
   return (
-    <div>
-      
+    <div className='skills-container'>
+      {frontend.map((skill) => (
+        <div>
+        <h4>{skill.name}</h4>
+        <img src={skill.icon} className='img-icons'/>
+        </div>
+      ))}
+      {backend.map((skill) => (
+        <div>
+        <h4>{skill.name}</h4>
+        <img src={skill.icon} className='img-icons'/>
+        </div>
+      ))}
     </div>
   )
 }
